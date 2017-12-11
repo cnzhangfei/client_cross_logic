21f02da06d77c459692ebe7c4c794a53c8b168fb
ShiHua <ShiHua@huaguangmeideMacBook-Pro.local>
2017-12-11 16:33:55 +0800

    增加codelite IDE支持

---
+++ prj/clproject/test/Debug/main.cpp.o.d
@@ -0,0 +1,41 @@
+./Debug/main.cpp.o: main.cpp ../../../test/cpp_test/test.h \
+  ../../../src/special_client.h ../../../src/details/commands.h \
+  ../../../src/details/../action_list.h ../../../src/details/client.h \
+  ../../../src/details/data_store.h ../../../src/details/action.h \
+  ../../../src/details/information.h ../../../src/details/item_def.h \
+  ../../../src/details/../item_list.h \
+  ../../../src/details/action_details.h ../../../src/details/debug.h \
+  ../../../src/details/agent.h ../../../src/details/concrete.h \
+  ../../../src/details/http_action.h ../../../src/details/gui_action.h
+
+../../../test/cpp_test/test.h:
+
+../../../src/special_client.h:
+
+../../../src/details/commands.h:
+
+../../../src/details/../action_list.h:
+
+../../../src/details/client.h:
+
+../../../src/details/data_store.h:
+
+../../../src/details/action.h:
+
+../../../src/details/information.h:
+
+../../../src/details/item_def.h:
+
+../../../src/details/../item_list.h:
+
+../../../src/details/action_details.h:
+
+../../../src/details/debug.h:
+
+../../../src/details/agent.h:
+
+../../../src/details/concrete.h:
+
+../../../src/details/http_action.h:
+
+../../../src/details/gui_action.h:
---
+++ prj/clproject/test/test.mk
@@ -0,0 +1,110 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=test
+ConfigurationName      :=Debug
+WorkspacePath          :=/Users/ShiHua/Downloads/workspace/client_cross_logic/prj/clproject
+ProjectPath            :=/Users/ShiHua/Downloads/workspace/client_cross_logic/prj/clproject/test
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=ShiHua
+Date                   :=11/12/2017
+CodeLitePath           :="/Users/ShiHua/Library/Application Support/codelite"
+LinkerName             :=/usr/bin/clang++
+SharedObjectLinkerName :=/usr/bin/clang++ -dynamiclib -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="test.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). $(IncludeSwitch)../../../src $(IncludeSwitch)../../../test/cpp_test 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/clang++
+CC       := /usr/bin/clang
+CXXFLAGS :=  -g -O0 -std=c++11 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/private/var/folders/tw/b5x_89951w574hvqxp4gxvwc0000gr/T/AppTranslocation/589644C7-9DD7-458D-8B1B-B92BCB733EB5/d/codelite.app/Contents/SharedSupport/
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+
+$(IntermediateDirectory)/.d:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/Users/ShiHua/Downloads/workspace/client_cross_logic/prj/clproject/test/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
